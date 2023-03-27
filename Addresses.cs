9981ad2bbbfe394f754cae62c76121f53e69717b
AleAlle62 <basketallegrini@gmail.com>
2023-03-27 10:18:33 +0200

    .

--- Addresses.cs
+++ Addresses.cs
@@ -4,9 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace csharp_lista_indrizzi
+namespace ListaIndirizzi
 {
-    internal class Addresses
+    public record Address
     {
+        public Address(string name, string surname, string street, string city, string province, string zip)
+        {
+            Name = name;
+            Surname = surname;
+            Street = street;
+            City = city;
+            Province = province;
+            Zip = zip;
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Zip { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using ListaIndirizzi;
+
+var addresses = Parser.Read();
+
+foreach (var address in addresses)
+{
+    Console.WriteLine(address);
+}
+
+
+
+
+// importare il file csv nel progetto
+// fare la classe adress
+// dentro la classe adress facciamo tutte le propriety delle stringhe 
+// fare il costruttore per inizializzarle 
+// fare un altra classe per le logiche del file
+// creare classe Parser
+// in Parser creare tutta la logica per la classe Adress
+// fare un controllo sulle stringhe in modo che se e' vuoto dia errore  
